7b5579b2d30053a9bc97a75fd876897e4f6059ee
ThanviChowdhary <thanvichowdhary10@gmail.com>
2023-09-08 16:17:32 +0530

    first commit

--- index.js
+++ index.js
@@ -8,40 +8,40 @@ app.use(bodyParser.json());
 
 // Sample user data
 const userData = {
-  user_id: 'john_doe_17091999',
-  email: 'john@xyz.com',
-  roll_number: 'ABCD123',
+    user_id: 'Thanvi Chowdhary B',
+    email: 'tb4994@srmist.edu.in',
+    roll_number: 'RA2011003020054',
 };
 
 // POST endpoint
 app.post('/bfhl', (req, res) => {
-  const requestBody = req.body;
+    const requestBody = req.body;
 
-  // Extract alphabets and find the highest alphabet
-  const alphabets = requestBody.data.filter((item) => typeof item === 'string' && item.match(/[A-Za-z]/));
-  const highestAlphabet = alphabets.length > 0 ? [alphabets.sort()[alphabets.length - 1]] : [];
+    // Extract alphabets and find the highest alphabet
+    const alphabets = requestBody.data.filter((item) => typeof item === 'string' && item.match(/[A-Za-z]/));
+    const highestAlphabet = alphabets.length > 0 ? [alphabets.sort()[alphabets.length - 1]] : [];
 
-  // Prepare the response
-  const response = {
-    is_success: true,
-    user_id: userData.user_id,
-    email: userData.email,
-    roll_number: userData.roll_number,
-    numbers: requestBody.data.filter((item) => typeof item === 'number'),
-    alphabets: alphabets,
-    highest_alphabet: highestAlphabet,
-  };
+    // Prepare the response
+    const response = {
+        is_success: true,
+        user_id: userData.user_id,
+        email: userData.email,
+        roll_number: userData.roll_number,
+        numbers: requestBody.data.filter((item) => typeof item === 'number'),
+        alphabets: alphabets,
+        highest_alphabet: highestAlphabet,
+    };
 
-  res.json(response);
+    res.json(response);
 });
 
 // GET endpoint
 app.get('/bfhl', (req, res) => {
-  // Respond with the operation_code
-  res.status(200).json({ operation_code: 1 });
+    // Respond with the operation_code
+    res.status(200).json({ operation_code: 1 });
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
